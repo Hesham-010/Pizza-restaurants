32b359d505bad561cfb94447443e1ee0e1a0de5b
Hesham-010 <heshamelshamy010@gmail.com>
2024-01-22 16:44:26 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.use(cookieParser());
+  const sessionConfig = session({
+    secret: 'somesupersecretsessionsecret',
+    resave: false,
+    saveUninitialized: false,
+    name: 'sessid',
+    cookie: {
+      maxAge: 2400000,
+      sameSite: 'strict',
+      httpOnly: true,
+      domain: 'localhost',
+      secure: false,
+    },
+  });
+  app.use(sessionConfig);
+
   await app.listen(3000);
 }
 bootstrap();
