6259cb2e0189fd21fab5e326d3d6db79973174a8
Hesham-010 <heshamelshamy010@gmail.com>
2024-01-22 17:08:56 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import * as csrf from 'csurf';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
-  app.use(csrf({ cookie: true }));
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    //Add your origins here
+    origin: 'https://pizza-restaurants-chd5.onrender.com/graphql',
+  });
 
   await app.listen(3000);
 }
