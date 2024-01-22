bbf276d97e1eecec0d6f847e06da284c45f669c2
Hesham-010 <heshamelshamy010@gmail.com>
2024-01-22 16:26:17 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as csurf from 'csurf';
-import * as session from 'express-session';
-import * as cookieParser from 'cookie-parser';
-import * as expressCsrf from 'express-csrf-protect';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.use(expressCsrf.enable());
-
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
