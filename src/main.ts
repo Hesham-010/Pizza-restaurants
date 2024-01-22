2b12cb63c9856db89a9a5bb00cd6540f5aef6eb4
Hesham-010 <heshamelshamy010@gmail.com>
2024-01-22 14:41:15 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as csurf from 'csurf';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.use(
-    csurf({ cookie: { httpOnly: true, secure: true, sameSite: 'strict' } }),
+    session({
+      secret: 'your-secret',
+      resave: false,
+      saveUninitialized: false,
+    }),
   );
+  app.use(csurf());
 
   await app.listen(3000);
 }
