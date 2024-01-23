140488e2aeb27c45f369aed55e9b44b48a9be9f2
Hesham-010 <heshamelshamy010@gmail.com>
2024-01-23 10:04:01 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,13 @@
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import cors, { CorsOptions } from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.enableCors({
     origin: true,
-    // CORS HTTP methods
     methods: ['POST'],
-  });
-
-  const corsOptions: CorsOptions = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  };
-
-  app.use('/graphql', (req, res, next) => {
-    cors(corsOptions)(req, res, () => {
-      next();
-    });
   });
 
   await app.listen(3000);
