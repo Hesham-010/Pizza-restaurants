244bcff538a63c506a0accf5977a7f033a8d37d6
Hesham-010 <heshamelshamy010@gmail.com>
2024-01-23 14:31:59 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { CustomHeaderMiddleware } from './_common/middlewares/headers';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({ origin: '*' });
-
-  app.use(new CustomHeaderMiddleware());
 
   await app.listen(3000);
 }
